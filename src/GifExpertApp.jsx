051cdd4dd40494057dbe0ce285a311f710957dbb
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-08 22:39:38 -0400

    feat: GifExpertApp component created

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,7 @@
+export const GifExpertApp = () => {
+  return (
+    <div className="bg-cyan-800 h-screen flex  flex-col items-center justify-center">
+      <h1 className="text-4xl text-white">GifExpertApp</h1>
+    </div>
+  );
+};
