8fd1f0b2e64b69d8842e712b490be2e80d9f9c1d
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-08 22:56:48 -0400

    +updates

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,13 @@
 export const GifExpertApp = () => {
   return (
-    <div className="bg-cyan-800 h-screen flex  flex-col items-center justify-center">
+    <div className="bg-cyan-800 h-screen flex flex-col p-10">
+      {/* Titulo */}
       <h1 className="text-4xl text-white">GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de GIFs */}
+      {/* GIF Item */}
     </div>
   );
 };
