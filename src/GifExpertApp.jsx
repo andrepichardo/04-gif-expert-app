a2c3dcd2bf37e22723431f0334ea4deac56d1ab6
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-13 11:29:20 -0400

    +updates

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch Man",
+    "Dragon Ball",
+  ]);
+
+  const onAddCategory = () => {};
+
   return (
-    <div className="bg-cyan-800 h-screen flex flex-col p-10">
+    <div className="bg-cyan-800 h-screen flex flex-col p-10 gap-4">
       {/* Titulo */}
       <h1 className="text-4xl text-white">GifExpertApp</h1>
-
       {/* Input */}
 
       {/* Listado de GIFs */}
+      <button className="self-start">Agregar</button>
+      <ol className="list-decimal list-inside text-white">
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
       {/* GIF Item */}
     </div>
   );
