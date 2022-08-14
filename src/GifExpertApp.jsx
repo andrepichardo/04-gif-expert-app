e29c395ced4ee8b7c8a0d1aa668b9344927d6e99
André Pichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-14 12:16:00 -0400

    +updates

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -6,21 +6,35 @@ export const GifExpertApp = () => {
     "Dragon Ball",
   ]);
 
-  const onAddCategory = () => {};
+  const onAddCategory = () => {
+    // setCategories([...categories, "Naruto"]);
+    setCategories(categories.concat("Naruto"));
+  };
 
   return (
     <div className="bg-cyan-800 h-screen flex flex-col p-10 gap-4">
       {/* Titulo */}
       <h1 className="text-4xl text-white">GifExpertApp</h1>
+
       {/* Input */}
+      <input
+        className="self-start px-2 py-1 rounded outline-none border border-cyan-400"
+        type="text"
+      />
 
       {/* Listado de GIFs */}
-      <button className="self-start">Agregar</button>
+      <button
+        onClick={onAddCategory}
+        className="active:scale-95 transition-all self-start bg-white text-cyan-600 rounded p-1"
+      >
+        Agregar
+      </button>
       <ol className="list-decimal list-inside text-white">
         {categories.map((category) => {
           return <li key={category}>{category}</li>;
         })}
       </ol>
+
       {/* GIF Item */}
     </div>
   );
