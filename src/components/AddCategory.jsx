d91c71f0009273e03ab18027e2a525d32f40138b
André Pichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-14 21:23:32 -0400

    Update AddCategory.jsx

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 export const AddCategory = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <input
         className="self-start px-2 py-1 rounded outline-none border border-cyan-400"
         type="text"
+        placeholder="Buscar GIFs"
+        value={inputValue}
+        onChange={onInputChange}
       />
-    </div>
+    </form>
   );
 };
