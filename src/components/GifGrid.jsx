2969ac6c0c2ab68a9da8bf3373827329e60e2848
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-16 19:43:19 -0400

    feat: useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <div className="flex flex-col">
       <h1 className="text-3xl">{category}</h1>
+      <p>{counter}</p>
+      <button
+        className="bg-stone-500 self-start p-3"
+        onClick={() => setCounter(counter + 1)}
+      >
+        +1
+      </button>
     </div>
   );
 };
