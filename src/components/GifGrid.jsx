20a204e9a10cec92e5021f8792b449328bab016b
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-08-20 11:54:16 -0400

    custom hook useFetchGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,23 +1,15 @@
-import { useState, useEffect } from "react";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    // getGifs(category).then((newImages) => setImages(newImages));
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <div className="flex flex-col gap-3 text-white">
       <h1 className="text-3xl mx-auto">{category}</h1>
+
+      {isLoading && <h2>Cargando...</h2>}
+
       <div className="grid sm:grid-cols-2 md:grid-cols-4 xl:gap-0 gap-3 mb-10">
         {images.map((image) => (
           <GifItem key={image.id} {...image} />
