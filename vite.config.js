abfd86a02ede3d6c19e6455ad6080d4ba859e561
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2022-10-21 22:47:22 -0400

    +rebuild docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/04-gif-expert-app/",
+  base: "/gif-expert-app/",
 });
